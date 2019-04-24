d8e6a6974fa3d5285803473329a7971ef159fc2f
Matt Levy <milevy1@gmail.com>
2019-04-24 13:02:47 -0600

    Refactored routes to handrolled ones for mod 3 intermission work

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,35 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome_page#index'
 
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, only: [:new, :create]
-  end
-  resources :authors, only: [:show, :destroy]
+  # Root landing page
+  get '/', to: 'welcome_page#index', as: :root
 
-  resources :reviews, only: [:show], as: 'user'
-  resources :reviews, only: [:destroy]
+  # Books
+  get     '/books',          to: 'books#index'
+  get     '/books/new',      to: 'books#new',   as: :new_book
+  get     '/books/:id',      to: 'books#show',  as: :book
+  post    '/books',          to: 'books#create'
+  get     '/books/:id/edit', to: 'books#edit',  as: :edit_book
+  patch   '/books/:id',      to: 'books#update'
+  delete  '/books/:id',      to: 'books#destroy'
+
+  # Reviews
+  get '/books/:book_id/reviews/new', to: 'reviews#new',     as: :new_book_review
+  post '/books/:book_id/reviews',    to: 'reviews#create',  as: :book_reviews
+  get '/review/:id',                 to: 'reviews#show',    as: :user
+  delete '/reviews/:id',             to: 'reviews#destroy', as: :review
+
+  # Authors
+  get '/authors/:id',    to: 'authors#show',  as: :author
+  delete '/authors/:id', to: 'authors#destroy'
+
+  # root 'welcome_page#index'
+
+  # resources :books do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  # resources :authors, only: [:show, :destroy]
+  #
+  # resources :reviews, only: [:show], as: 'user'
+  # resources :reviews, only: [:destroy]
 end
